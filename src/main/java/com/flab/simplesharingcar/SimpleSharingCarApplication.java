49b2bf486e65d0ba5e366727df425c9ff4235eb9
KTG5672 <zzang5992@gmail.com>
2023-05-05 01:56:30 +0900

    project init

---
+++ src/main/java/com/flab/simplesharingcar/SimpleSharingCarApplication.java
@@ -0,0 +1,13 @@
+package com.flab.simplesharingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSharingCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSharingCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/simplesharingcar/SimpleSharingCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.simplesharingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSharingCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

56e7249d7f3a1351b61adf55683f438c7897db5a
KTG5672 <zzang5992@gmail.com>
2023-05-09 00:50:24 +0900

    feat : User Repository 저장 기능 작성 및 테스트

--- src/main/java/com/flab/simplesharingcar/domain/User.java
+++ src/main/java/com/flab/simplesharingcar/domain/User.java
@@ -1,10 +1,12 @@
 package com.flab.simplesharingcar.domain;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @EqualsAndHashCode
+@Builder
 public class User {
 
     private Long id;
@@ -14,5 +16,13 @@ public class User {
     private String password;
 
     private String name;
+
+    public static User createUser(String email, String password, String name) {
+        return User.builder()
+            .email(email)
+            .password(password)
+            .name(name)
+            .build();
+    }
 
 }
---
+++ src/test/java/com/flab/simplesharingcar/repository/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.flab.simplesharingcar.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.flab.simplesharingcar.domain.User;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.jdbc.Sql;
+
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@MybatisTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeTestClass
+    @Sql({"classpath:db/mysql/schema.sql"})
+    public void initClass() {}
+
+    @Test
+//    @Rollback(value = false)
+    public void 유저_저장_테스트() {
+        // given
+        User givenUser = User.createUser("gasdkwo@gafsa.com", "11234", "김태경");
+        // when
+        Long savedId = userRepository.save(givenUser);
+        // then
+        assertThat(savedId).isEqualTo(1L);
+    }
+}

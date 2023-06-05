1e5a6bd774adf446cd1f79f6fca4ba3a5b393658
KTG5672 <zzang5992@gmail.com>
2023-06-06 03:11:17 +0900

    feat : 주변 주차장 검색 구현

---
+++ src/main/java/com/flab/simplesharingcar/service/sharing/SharingZoneService.java
@@ -0,0 +1,61 @@
+package com.flab.simplesharingcar.service.sharing;
+
+import com.flab.simplesharingcar.domain.Location;
+import com.flab.simplesharingcar.domain.SharingZone;
+import com.flab.simplesharingcar.repository.SharingZoneRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation;
+import org.springframework.data.redis.core.GeoOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SharingZoneService {
+
+    private static final String SHARING_ZONE = "sharing_zone";
+
+    private final GeoOperations<String, Object> geoOperations;
+
+    private final SharingZoneRepository sharingZoneRepository;
+
+    public SharingZone save(SharingZone sharingZone) {
+        String name = sharingZone.getName();
+        Location location = sharingZone.getLocation();
+
+        SharingZone saveSharingZone = SharingZone.builder()
+            .name(name)
+            .location(location)
+            .build();
+        Point pointByLocation = saveSharingZone.getPointByZoneLocation();
+
+        sharingZoneRepository.save(saveSharingZone);
+        geoOperations.add(SHARING_ZONE, pointByLocation, saveSharingZone);
+
+        return saveSharingZone;
+    }
+
+    public List<SharingZone> findByLocation(Double latitude, Double longitude, Double distance) {
+        Point center = new Point(latitude, longitude);
+        Distance distanceKm = new Distance(distance, Metrics.KILOMETERS);
+        Circle inner = new Circle(center, distanceKm);
+
+        GeoResults<GeoLocation<Object>> results = geoOperations.radius(SHARING_ZONE, inner);
+        List<GeoResult<GeoLocation<Object>>> contents = results.getContent();
+        List<SharingZone> findZones = contents.stream()
+            .map(GeoResult::getContent)
+            .map(GeoLocation::getName)
+            .map(o -> (SharingZone) o)
+            .collect(Collectors.toList());
+        return findZones;
+    }
+}
--- src/main/java/com/flab/simplesharingcar/web/controller/HomeController.java
+++ src/main/java/com/flab/simplesharingcar/web/controller/HomeController.java
@@ -1,16 +1,40 @@
 package com.flab.simplesharingcar.web.controller;
 
+import com.flab.simplesharingcar.domain.SharingZone;
+import com.flab.simplesharingcar.service.sharing.SharingZoneService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/home")
 public class HomeController {
+
+    private final SharingZoneService sharingZoneService;
 
     @GetMapping
     public String home() {
         return "home";
     }
 
+    @PostMapping("/testSave")
+    public ResponseEntity<SharingZone> save(@RequestBody SharingZone sharingZone) {
+        return ResponseEntity.ok(sharingZoneService.save(sharingZone));
+    }
+
+    @GetMapping("/testSearch")
+    public ResponseEntity<List<SharingZone>> search(
+        @RequestParam("lng") Double lng
+        , @RequestParam("lat") Double lat
+        , @RequestParam("km") Double km
+    ) {
+        return ResponseEntity.ok(sharingZoneService.findByLocation(lat, lng, km));
+    }
 }
